7f687d6e7a47e6fdfcd684935e68aab1bab1a004
Abhishek Gupta <abhishekgupta311287@gmail.com>
2021-02-08 14:33:56 +0530

    StocksViewModel implementation with repo call required for inflating data on home screen with list of stocks

--- app/src/main/java/com/abhishekgupta/stocks/viewmodel/StocksViewModel.kt
+++ app/src/main/java/com/abhishekgupta/stocks/viewmodel/StocksViewModel.kt
@@ -1,7 +1,39 @@
 package com.abhishekgupta.stocks.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abhishekgupta.stocks.model.Resource
+import com.abhishekgupta.stocks.model.Stock
+import com.abhishekgupta.stocks.repo.IStocksRepo
+import kotlinx.coroutines.launch
 
-class StocksViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class StocksViewModel(private val repo: IStocksRepo) : ViewModel() {
+    val stocksLiveData: MutableLiveData<List<Stock>> = MutableLiveData()
+
+    fun getStockQuotes() {
+        viewModelScope.launch {
+            Resource.Loading(null)
+            val (success, data, error, errorType) = repo.getStockQuotes(
+                listOf(
+                    "RELI",
+                    "TCS",
+                    "ITC",
+                    "HDBK",
+                    "INFY"
+                )
+            )
+
+            if (success && data?.isNotEmpty() == true) {
+                Resource.Success(data)
+            } else {
+                Resource.Error("$error - $errorType", data)
+            }
+
+        }
+    }
+
+    fun listenToStockQuotes() {
+        // TODO : implementation for play button
+    }
 }
