c2400e904e66735d040834311721ac3dfe2c6cdf
Abhishek Gupta <abhishekgupta311287@gmail.com>
2021-02-08 21:13:45 +0530

    Minor corrections

--- app/src/main/java/com/abhishekgupta/stocks/view/adapter/StockViewHolder.kt
+++ app/src/main/java/com/abhishekgupta/stocks/view/adapter/StockViewHolder.kt
@@ -11,13 +11,13 @@ class StockViewHolder(view: View) : BaseViewHolder<Stock>(view) {
     override fun bind(t: Stock) {
         val binding = StocksItemLayoutBinding.bind(itemView)
         binding.sid.text = t.sid
-        binding.price.text = itemView.resources.getString(R.string.sid_price, t.price)
+        binding.price.text = itemView.resources.getString(R.string.sid_price, t.price.toString())
 
         binding.changeIcon.setImageResource(
             if (t.change > 0) {
                 R.drawable.green_stock_up_icon
             } else {
-                R.drawable.green_stock_up_icon
+                R.drawable.red_stock_down_icon
             }
         )
     }
