671d3e1f8b375972744d557a38c08d77da648720
Abhishek Gupta <abhishekgupta311287@gmail.com>
2021-02-08 14:13:43 +0530

    Added error fields in Quote class and added Resource.kt for notifying the state to ui.

--- app/src/main/java/com/abhishekgupta/stocks/model/Quote.kt
+++ app/src/main/java/com/abhishekgupta/stocks/model/Quote.kt
@@ -2,5 +2,7 @@ package com.abhishekgupta.stocks.model
 
 data class Quote(
     val success: Boolean,
-    val data: List<Stock>
+    val data: List<Stock>?,
+    val error: String?,
+    val errorType: String?
 )
